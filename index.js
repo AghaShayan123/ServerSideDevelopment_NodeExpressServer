c22fe27e83420a74e0ab747bdecd36c52acf7d10
unknown <agha.shayan123@gmail.com>
2021-07-10 20:00:54 +0500

    Express Router

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan')
-const bodyParser = require('body-parser')
 
+const dishRouter = require('./routes/dishRouter')
 const hostname = 'localhost';
 const port = 3000;
 
@@ -12,28 +12,7 @@ app.use(express.json())
 
 app.use(express.static(__dirname + '/public'))
 
-app.all('/dishes', (req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain')
-    next()
-})
-
-app.get('/dishes', (req, res, next) => {
-    res.end('Will send details of all dishes')
-})
-
-app.post('/dishes', (req, res, next) => {
-    res.end(`Will add  the dish: ${req.body.name} with details ${req.body.description}`)
-})
-
-app.put('/dishes', (req, res, next) => {
-    res.statusCode = 403
-    res.end('Put operation not supported on dishes')
-})
-
-app.delete('/dishes', (req, res, next) => {
-    res.end('Delete all dishes')
-})
+app.use('/dishes', dishRouter)
 
 app.get('/dishes/:dishId', (req, res, next) => {
     res.end(`Will send details of dish ${req.params.dishId} to you!`)
